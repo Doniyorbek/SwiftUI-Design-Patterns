21dbefdf1e86dc461efe07d37865029d3a39ccb8
Doniyorbek <doniyorbek@aloqabank.uz>
2021-01-06 11:03:14 +0500

    first commit

---
+++ SwiftUI-Clean-Architecture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Clean-Architecture
+//
+//  Created by Doniyorbek on 12/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Clean-Architecture/SwiftUI_Clean_ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Clean_ArchitectureApp.swift
+//  SwiftUI-Clean-Architecture
+//
+//  Created by Doniyorbek on 12/19/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Clean_ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
